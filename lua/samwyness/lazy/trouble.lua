81cc12b16863b5c5d8539ea4b7b4080f4c13cd00
samwyness <samwyness22@gmail.com>
2024-06-11 21:19:12 +1000

    cleanup trouble

--- lua/samwyness/lazy/trouble.lua
+++ lua/samwyness/lazy/trouble.lua
@@ -1,22 +1,20 @@
 return {
-  {
-    'folke/trouble.nvim',
-    config = function()
-      require('trouble').setup {
-        icons = false,
-      }
+  'folke/trouble.nvim',
+  config = function()
+    require('trouble').setup {
+      icons = false,
+    }
 
-      vim.keymap.set('n', '<leader>tt', function()
-        require('trouble').toggle()
-      end)
+    vim.keymap.set('n', '<leader>tt', function()
+      require('trouble').toggle()
+    end, { desc = 'Toggle the trouble list' })
 
-      vim.keymap.set('n', '[t', function()
-        require('trouble').next { skip_groups = true, jump = true }
-      end)
+    vim.keymap.set('n', '<leader>tn', function()
+      require('trouble').next { skip_groups = true, jump = true }
+    end, { desc = 'Go to the next diagnostic in the trouble list' })
 
-      vim.keymap.set('n', ']t', function()
-        require('trouble').previous { skip_groups = true, jump = true }
-      end)
-    end,
-  },
+    vim.keymap.set('n', '<leader>tp', function()
+      require('trouble').previous { skip_groups = true, jump = true }
+    end, { desc = 'Go to the previous diagnostic in the trouble list' })
+  end,
 }
