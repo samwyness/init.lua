958b8e9d04de1ffbf38d33c5eb27c43cbe941286
samwyness <samwyness22@gmail.com>
2024-09-30 16:54:50 +0200

    override catpuccin theme defaults

--- lua/samwyness/lazy/colors.lua
+++ lua/samwyness/lazy/colors.lua
@@ -31,6 +31,18 @@ return {
     'catppuccin/nvim',
     name = 'catppuccin',
     priority = 1000,
+    config = function()
+      require('catppuccin').setup {
+        flavour = 'auto', -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+          light = 'latte',
+          dark = 'macchiato',
+        },
+        no_italic = true, -- Force no italic
+        no_bold = true, -- Force no bold
+        no_underline = true, -- Force no underline
+      }
+    end,
   },
 
   {
