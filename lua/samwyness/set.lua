3b71a1886437b54e79c9c2783d8a402af0c484d7
samwyness <samwyness22@gmail.com>
2024-09-30 16:52:24 +0200

    set default tab values

--- lua/samwyness/set.lua
+++ lua/samwyness/set.lua
@@ -18,8 +18,8 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- Tabs & indentation
--- vim.opt.tabstop = 2
--- vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
