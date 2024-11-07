cd650df468bf81d8c843969399655bb056d6a9f5
samwyness <samwyness@MEMB036.local>
2024-11-07 12:14:29 +1000

    Remove harpoon telescope integration

--- lua/samwyness/lazy/harpoon.lua
+++ lua/samwyness/lazy/harpoon.lua
@@ -13,68 +13,8 @@ return {
       harpoon:list():add()
     end, { desc = '[A]dd current buffer to harpoon' })
 
-    -- vim.keymap.set('n', '<C-e>', function()
-    --   harpoon.ui:toggle_quick_menu(harpoon:list())
-    -- end)
-
-    vim.keymap.set('n', '<C-S-P>', function()
-      harpoon:list():prev()
-    end)
-
-    vim.keymap.set('n', '<C-S-N>', function()
-      harpoon:list():next()
-    end)
-
-    -- basic telescope configuration
-    local conf = require('telescope.config').values
-    local function toggle_telescope(harpoon_files)
-      local make_finder = function()
-        local paths = {}
-        for _, item in ipairs(harpoon_files.items) do
-          table.insert(paths, item.value)
-        end
-
-        return require('telescope.finders').new_table {
-          results = paths,
-        }
-      end
-
-      require('telescope.pickers')
-        .new({}, {
-          prompt_title = 'Harpoon',
-          finder = make_finder(),
-          previewer = conf.file_previewer {},
-          sorter = conf.generic_sorter {},
-          attach_mappings = function(prompt_buffer_number, map)
-            map('i', '<C-d>', function()
-              local state = require 'telescope.actions.state'
-              local selected_entry = state.get_selected_entry()
-              local current_picker = state.get_current_picker(prompt_buffer_number)
-
-              harpoon:list():remove_at(selected_entry.index)
-              current_picker:refresh(make_finder())
-            end)
-
-            return true
-          end,
-        })
-        :find()
-    end
-
     vim.keymap.set('n', '<C-e>', function()
-      toggle_telescope(harpoon:list())
-    end, { desc = 'Open harpoon window' })
-
-    harpoon:extend {
-      UI_CREATE = function(cx)
-        vim.keymap.set('n', '<C-v>', function()
-          harpoon.ui:select_menu_item { vsplit = true, toright = true }
-        end, { buffer = cx.bufnr })
-
-        vim.keymap.set('n', '<C-s>', function()
-          harpoon.ui:select_menu_item { split = true }
-        end, { buffer = cx.bufnr })
-      end,
-    }
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end)
   end,
 }
